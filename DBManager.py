9e2e56bfc1acfd154b1bf4c503bb887ace765a1f
Tri Huynh <trihuynh87@gmail.com>
2013-06-30 22:33:41 -0700

    Separated db actions to DBManage, added addDevice.html for testing

---
+++ DBManager.py
@@ -0,0 +1,350 @@
+#!/usr/bin/python
+"""
+Python script for managing database
+"""
+import MySQLdb
+
+
+# Function to create MySQL database connection
+def getDBCon():
+	return MySQLdb.connect(host="localhost", # your host, usually localhost
+		                   user="ceng499", # your username
+		                   passwd="ceng499", # your password
+		                   db="pihome") # name of the data base
+		                        
+# Function to create fresh database tables
+def initDatabase():
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Create Devices table, drop previous table if existed
+	dbcur.execute("SHOW TABLES LIKE 'Devices'")
+	result=dbcur.fetchone()
+	if not (result is None or result[0] is None):
+		dbcur.execute("DROP TABLE Devices")
+	dbcur.execute("CREATE TABLE Devices(serial BIGINT UNSIGNED PRIMARY KEY, type TINYINT, name VARCHAR(20), status TINYINT, active TINYINT)")
+
+	# Create Credential table, drop previous table if existed
+	dbcur.execute("SHOW TABLES LIKE 'Credential'")
+	result=dbcur.fetchone()
+	if not (result is None or result[0] is None):
+		dbcur.execute("DROP TABLE Credential")
+	dbcur.execute("CREATE TABLE Credential(username VARCHAR(20) PRIMARY KEY, password VARCHAR(20))")
+	dbcur.execute("INSERT INTO Credential VALUES('pihome','pihomepass')")
+
+	# Create Emails table, drop previous table if existed
+	dbcur.execute("SHOW TABLES LIKE 'Emails'")
+	result=dbcur.fetchone()
+	if not (result is None or result[0] is None):
+		dbcur.execute("DROP TABLE Emails")
+	dbcur.execute("CREATE TABLE Emails(email VARCHAR(20) PRIMARY KEY)")
+
+	# Create Nodes table, drop previous table if existed
+	dbcur.execute("SHOW TABLES LIKE 'Nodes'")
+	result=dbcur.fetchone()
+	if not (result is None or result[0] is None):
+		dbcur.execute("DROP TABLE Nodes")
+	dbcur.execute("CREATE TABLE Nodes(nodename VARCHAR(10) PRIMARY KEY, ipadd VARCHAR(15), port SMALLINT UNSIGNED)")
+	
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to add new device
+def addDevice(_serial, _type, _name, _status, _active):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device is already existed
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		# Insert new entry to Devices table
+		dbcur.execute("INSERT INTO Devices VALUES(%s,%s,%s,%s,%s)", (_serial,_type,_name,_status,_active))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to remove device
+def removeDevice(_serial):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Remove device if existed
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		dbcur.execute("DELETE FROM Devices WHERE serial=%s", _serial)
+		
+	# Commit, close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+	
+# Function to get device information
+def getDevice(_serial):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device exists
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		# Return device info
+		return result
+		
+	# Close db cursor, connection
+	dbcur.close()
+	dbcon.close()
+
+# Function to get all devices
+def getDevices():
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Find all devices
+	dbcur.execute("SELECT * FROM Devices")
+	results=dbcur.fetchall()
+	return results
+		
+	# Close db cursor, connection
+	dbcur.close()
+	dbcon.close()
+	
+# Function to update device status
+def updateDeviceStatus(_serial, _status):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device exists
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		# Update status of device
+		dbcur.execute("UPDATE Devices SET status=%s WHERE serial=%s", (_status,_serial))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to activate or deactivate device
+def updateDeviceActive(_serial, _active):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device exists
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		# Update active of device
+		dbcur.execute("UPDATE Devices SET active=%s WHERE serial=%s", (_active,_serial))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+	
+# Function to change device name
+def changeDeviceName(_serial, _name):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device exists
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		# Change name of device
+		dbcur.execute("UPDATE Devices SET name=%s WHERE serial=%s", (_name,_serial))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to change device type
+def changeDeviceType(_serial, _type):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if device exists
+	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Device %s does not exist" % _serial)
+	else:		
+		# Change type of device
+		dbcur.execute("UPDATE Devices SET type=%s WHERE serial=%s", (_type,_serial))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+	
+# Function to change password
+def changePassword(_user, _oldpass, _newpass):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Check if user credential is correct
+	dbcur.execute("SELECT * FROM Credential WHERE username=%s AND password=%s", (_user,_oldpass))
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Incorrect username and password %s %s" % (_user,_oldpass))
+	else:		
+		# Update new password
+		dbcur.execute("UPDATE Credential SET password=%s WHERE username=%s", (_newpass,_user))
+		
+	# Commit querry and close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to get all emails
+def getEmails():
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Find all emails
+	dbcur.execute("SELECT * FROM Emails")
+	results=dbcur.fetchall()
+	return results
+		
+	# Close db cursor, connection
+	dbcur.close()
+	dbcon.close()
+
+# Function to add email
+def addEmail(_email):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Add new email if not existed
+	dbcur.execute("SELECT email FROM Emails WHERE email=%s", _email)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		dbcur.execute("INSERT INTO Emails VALUES(%s)", _email)
+	# Close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to remove email
+def removeEmail(_email):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Remove email if existed
+	dbcur.execute("SELECT email FROM Emails WHERE email=%s", _email)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Email %s does not exist" % _email)
+	else:
+		dbcur.execute("DELETE FROM Emails WHERE email=%s", _email)
+	# Close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to get node information
+def getNode(_nodename):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Get node info if existed
+	dbcur.execute("SELECT * FROM Nodes WHERE nodename=%s", _nodename)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Node %s does not exist" % _nodename)
+	else:
+		return result
+
+	# Close db cursor, connection
+	dbcur.close()
+	dbcon.close()
+			
+# Function to add new node
+def addNode(_nodename, _ipadd, _port=80):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Add new node if not existed
+	dbcur.execute("SELECT * FROM Nodes WHERE nodename=%s", _nodename)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		dbcur.execute("INSERT INTO Nodes VALUES(%s,%s,%s)", (_nodename,_ipadd,_port))
+
+	# Commit, close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to remove node
+def removeNode(_nodename):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Remove node if existed
+	dbcur.execute("SELECT * FROM Nodes WHERE nodename=%s", _nodename)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Node %s does not exist" % _nodename)
+	else:
+		dbcur.execute("DELETE FROM Nodes WHERE nodename=%s", _nodename)
+
+	# Commit, close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+
+# Function to update node ipaddress and port
+def updateNode(_nodename, _ipadd, _port):
+	# Get database connection and cursor
+	dbcon = getDBCon()
+	dbcur = dbcon.cursor()
+	
+	# Update node if existed
+	dbcur.execute("SELECT * FROM Nodes WHERE nodename=%s", _nodename)
+	result=dbcur.fetchone()
+	if result is None or result[0] is None:
+		print("Node %s does not exist" % _nodename)
+	else:
+		dbcur.execute("UPDATE Nodes SET ipadd=%s, port=%s WHERE nodename=%s", (_ipadd,_port,_nodename))
+
+	# Commit, close db cursor, connection
+	dbcon.commit()
+	dbcur.close()
+	dbcon.close()
+	
+	
+	
+	
--- SerialMonitor.py
+++ SerialMonitor.py
@@ -1,274 +1,51 @@
 #!/usr/bin/python
 """
-Python script for managing database
-
--- Type 0 switch (on/off), 1 door sensor (open/close) for TRUE/FALSE
-DROP TABLE Devices;
-CREATE TABLE Devices(serial BIGINT UNSIGNED PRIMARY KEY, type TINYINT, name VARCHAR(20), status TINYINT, active TINYINT);
-INSERT INTO Devices VALUES(10000000000,0,'bedroom lamp',0, 1);
-INSERT INTO Devices VALUES(10000000001,1,'front door',0, 0);
-
-DROP TABLE Credential;
-CREATE TABLE Credential(username VARCHAR(20) PRIMARY KEY, password VARCHAR(20));
-INSERT INTO Credential VALUES('rpi','rpipass');
-
-DROP TABLE Emails;
-CREATE TABLE Emails(email VARCHAR(20) PRIMARY KEY);
-
---type: 0 public, 1 master, 2 slave
-DROP TABLE Nodes;
-CREATE TABLE Nodes(ipaddress VARCHAR(16) PRIMARY KEY, type TINYINT, mjpgport SMALLINT UNSIGNED);
-INSERT INTO Nodes VALUES('24.68.152.172',0,NULL);
-INSERT INTO Nodes VALUES('192.168.0.190',1,8080);
-INSERT INTO Nodes VALUES('192.168.0.191',2,8081);
+Python script for monitoring serial input and updating database as needed
 """
-
-import MySQLdb
-
-# Function to return MySQL database connection
-def getDBCon():
-	return MySQLdb.connect(host="localhost", # your host, usually localhost
-		                   user="ceng499", # your username
-		                   passwd="ceng499", # your password
-		                   db="pihome") # name of the data base
-		                        
-# Function to create fresh database tables
-def initDatabase():
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Create Devices table
-	dbcur.execute("DROP TABLE Devices")
-	dbcur.execute("CREATE TABLE Devices(serial BIGINT UNSIGNED PRIMARY KEY, type TINYINT, name VARCHAR(20), status TINYINT, active TINYINT)")
-
-	# Create Credential table
-	dbcur.execute("DROP TABLE Credential")
-	dbcur.execute("CREATE TABLE Credential(username VARCHAR(20) PRIMARY KEY, password VARCHAR(20))")
-	dbcur.execute("INSERT INTO Credential VALUES('pihome','pihomepass')")
-
-	# Create Emails table
-	dbcur.execute("DROP TABLE Emails")
-	dbcur.execute("CREATE TABLE Emails(email VARCHAR(20) PRIMARY KEY)")
-
-	# Create Nodes table
-	dbcur.execute("DROP TABLE Nodes")
-	dbcur.execute("CREATE TABLE Nodes(ipaddress VARCHAR(16) PRIMARY KEY, type TINYINT, mjpgport SMALLINT UNSIGNED)")
-	
-	# Commit querry and close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to add new device
-def addDevice(_serial, _type, _name, _status, _active):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Check if device is already existed
-	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		# Insert new entry to Devices table
-		dbcur.execute("INSERT INTO Devices VALUES(%s,%s,%s,%s,%s)", (_serial,_type,_name,_status,_active))
-		
-	# Commit querry and close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to get device information
-def getDevice(_serial):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Check if device exists
-	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Device %s does not exist" % _serial)
-	else:		
-		# Return device info
-		return result
-		
-	# Close db cursor, connection
-	dbcur.close()
-	dbcon.close()
-	
-# Function to update device status
-def updateDeviceStatus(_serial, _status):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Check if device exists
-	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Device %s does not exist" % _serial)
-	else:		
-		# Update status of device
-		dbcur.execute("UPDATE Devices SET status=%s WHERE serial=%s", (_status,_serial))
-		
-	# Commit querry and close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to change device name
-def changeDeviceName(_serial, _name):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Check if device exists
-	dbcur.execute("SELECT * FROM Devices WHERE serial=%s", _serial)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Device %s does not exist" % _serial)
-	else:		
-		# Change name of device
-		dbcur.execute("UPDATE Devices SET name=%s WHERE serial=%s", (_name,_serial))
-		
-	# Commit querry and close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to change password
-def changePassword(_user, _oldpass, _newpass):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Check if user credential is correct
-	dbcur.execute("SELECT * FROM Credential WHERE username=%s AND password=%s", (_user,_oldpass))
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Incorrect username and password %s %s" % (_user,_oldpass))
-	else:		
-		# Update new password
-		dbcur.execute("UPDATE Credential SET password=%s WHERE username=%s", (_newpass,_user))
-		
-	# Commit querry and close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to get emails
-def getEmails():
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Find all emails
-	dbcur.execute("SELECT * FROM Emails")
-	results=dbcur.fetchall()
-	return results
-		
-	# Close db cursor, connection
-	dbcur.close()
-	dbcon.close()
-
-# Function to add email
-def addEmail(_email):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Add new email if not existed
-	dbcur.execute("SELECT email FROM Emails WHERE email=%s", _email)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		dbcur.execute("INSERT INTO Emails VALUES(%s)", _email)
-	# Close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to remove email
-def removeEmail(_email):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Remove email if existed
-	dbcur.execute("SELECT email FROM Emails WHERE email=%s", _email)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Email %s does not exist" % _email)
-	else:
-		dbcur.execute("DELETE FROM Emails WHERE email=%s", _email)
-	# Close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-		
-# Function to add new node
-def addNode(_ipadd, _type, _port):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Add new node if not existed
-	dbcur.execute("SELECT * FROM Nodes WHERE ipaddress=%s", _ipadd)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		dbcur.execute("INSERT INTO Nodes VALUES(%s,%s,%s)", (_ipadd,_type,_port))
-
-	# Commit, close db cursor, connection
-	dbcon.commit()
-	dbcur.close()
-	dbcon.close()
-
-# Function to get node information
-def getNode(_ipadd):
-	# Get database connection and cursor
-	dbcon = getDBCon()
-	dbcur = dbcon.cursor()
-	
-	# Get node info if existed
-	dbcur.execute("SELECT * FROM Nodes WHERE ipaddress=%s", _ipadd)
-	result=dbcur.fetchone()
-	if result is None or result[0] is None:
-		print("Node %s does not exist" % _ipadd)
-	else:
-		return result
-
-	# Close db cursor, connection
-	dbcur.close()
-	dbcon.close()
+import DBManager as db
 		
 ####### Main
-initDatabase()
+db.initDatabase()
 
-addDevice(1000000000,1,'tridevice',0,1)
+db.addDevice(1000000000,1,'device1',0,1)
+db.addDevice(1000000000,2,'device1',0,1)
+db.addDevice(1000000001,0,'device2',1,1)
+db.removeDevice(1000000002)
+db.removeDevice(1000000003)
 
-updateDeviceStatus(1000000000,1)
-updateDeviceStatus(1000000001,1)
-changeDeviceName(1000000000,'trideptrai')
-changeDeviceName(1000000001,'yolo')
-device1 = getDevice(1000000000)
+db.updateDeviceStatus(1000000000,1)
+db.updateDeviceStatus(1000000001,1)
+db.updateDeviceActive(1000000001,1)
+db.updateDeviceActive(1000000002,1)
+db.changeDeviceName(1000000000,'trideptrai')
+db.changeDeviceName(1000000001,'yolo')
+db.changeDeviceType(1000000000,2)
+db.changeDeviceType(1000000001,1)
+device1 = db.getDevice(1000000000)
 print device1
-device2 = getDevice(1000000001)
+device2 = db.getDevice(1000000001)
 print device2
-changePassword('pihome','pihomepass','haha')
-changePassword('pihome','pihomepass','haha')
+db.changePassword('pihome','pihomepass','haha')
+db.changePassword('pihome','pihomepass','haha')
 
-addEmail('trihuynh87')
-addEmail('trihuynh877')
-removeEmail('trihuynh')
-removeEmail('trihuynh877')
+db.addEmail('trihuynh87')
+db.addEmail('trihuynh877')
+db.removeEmail('trihuynh')
+#removeEmail('trihuynh877')
 
-emails=getEmails()
+emails=db.getEmails()
 for email in emails:
 	print email
 
-addNode('192.168.1.12',0,8080)
-print getNode('192.168.1.1')
-nodeinfo=getNode('192.168.1.12')
-print nodeinfo[0]
+db.addNode('router','24.52.152.172')
+db.addNode('master','192.168.1.12',8080)
+db.addNode('camera1','192.168.1.11',8081)
+db.updateNode('camera1','192.168.1.15',8082)
+db.removeNode('router')
+print db.getNode('router')
+nodeinfo=db.getNode('camera1')
+print nodeinfo
 
-
+devices=db.getDevices()
+for device in devices:
+	print device
