8b9085cb1bf84831c3ab308aa37869f3db1dd18e
Tri Huynh <trihuynh87@gmail.com>
2013-07-06 21:15:17 -0700

    sendmail was working but not now

--- SerialMonitor.py
+++ SerialMonitor.py
@@ -55,33 +55,160 @@ Scope:
     		Update WebUI by sending internal URL 'refresh' request
     	
 """
+
+# Python packages
+import sys
+import urllib2
+import time;
+
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+# Required packages
 import DBManager as db
 		
-
-
+          
 ######################## Functions
+           
+# Function to send email
+def sendEmail(_email, _dname, _date, _link):
+	msg = MIMEText("Hello, we have detected your %s was opened at %s.\n \
+	Please click below for live stream update.\n %s" % (_dname,_date,_link))
+	msg["From"] = "minhtri@uvic.ca"
+	msg["To"] = _email
+	msg["Subject"] = "Yo Notification from UVicPiHome"
+	p = Popen(["/usr/sbin/sendmail", "-toi"], stdin=PIPE)
+	p.communicate(msg.as_string())
 
 
-# Function to parse for Serial Data
-#def parseSerialData():
-	# empty
+# Function to submit url to refresh Apache server
+def refreshUI():
+    base = 'home'
+    arg_string = 'Yo=Lo&Ha=y'
+    url = "https://localhost/" + base + "?" + arg_string
+    print url
+    
+    #Performs the actual submission of a URL generated for a command.
+    #urllib2.urlopen(url)
 
 
-# Function to process Serial Data
-def processSerialRequest(_serial, _data):
+# Function to determine Event, assuming input parameters are in correct format
+# _serial: 1000000000 (16 bit big int number)
+# _data: 0 for off, 1 for on
+def processEvent(_serial, _status):
 	# Extract device info based on serial number <dserial, dtype, dname, dstatus, dactive>
-	device=db.getDevice(_serial)
-
+	device = db.getDevice(_serial)
+	
+	# Current device status (0 for off, 1 for on)
+	newStatus = _status
+	
 	# Perform actions if device is valid
-	#if device is not None:
+	if device is not None:
 		# Extract device info
+		dserial = device[0]
+		dtype = device[1]
+		dname = device[2]
+		dstatus = device[3]
+		dactive = device[4]
+		
+		# Debug
+		print "Current device info: %s %s %s %s %s" % (dserial,dtype,dname,dstatus,dactive)
+		print "New device status: %s" % newStatus
+		
+		# First sanitize database data
+		if dtype != 0 and dtype != 1:
+			print 'Unknown device type: %s' % dtype
+			return 'DB_ERROR'
+		if dstatus != 0 and dstatus != 1:
+			print 'Unknown device status: %s' % dstatus
+			return 'DB_ERROR'
+		if dactive != 0 and dactive != 1:
+			print 'Unknown device active: %s' % dactive
+			return 'DB_ERROR'
+		
+		# If new status is the same as current status, do nothing
+		if dstatus == newStatus: return 'UNCHANGE'
+		
+		# Switch action
+		if dtype == 0: event = 'SWITCH_UPDATE'
+	
+		# Door action
+		if dtype == 1 and dactive == 0: event = 'DOOR_UPDATE'
+		if dtype == 1 and dactive == 1 and dstatus == 1 and newStatus == 0: event = 'DOOR_CLOSE'
+		if dtype == 1 and dactive == 1 and dstatus == 0 and newStatus == 1: event = 'DOOR_OPEN'
+		
+	# Cant find device serial
+	else:
+		print 'Device %s is not found' % _serial
+		return 'NO_DEVICE'
+
+
+	if event == 'SWITCH_UPDATE' or event == 'DOOR_UPDATE' or event == 'DOOR_CLOSE':
+		print 'UPDATE'
+		# Update DB
+		db.updateDeviceStatus(_serial,_status)
+		
+		# Send WebUI refresh request
+		refreshUI()
+			
+	if event == 'DOOR_OPEN':
+		print 'DOOR_OPEN'
+		# Update DB
+		db.updateDeviceStatus(_serial,_status)
+		
+		# Send WebUI refresh request
+		refreshUI()
+		
+		# Send email notifications
+		localtime = time.asctime(time.localtime(time.time()))
+		print localtime
+		router_ip=db.getNode('router')[1]
+ 		link = "http://%s/camera" % router_ip
+ 		print link
+		emails = db.getEmails()
+		for email in emails:
+			print email[0]
+			sendEmail(email[0],dname,localtime,link)
+			#sendMail()
+		
+		# Perform camera actions
+		
+	
+	
 
 ####### Main
-db.initDatabase()
+#db.initDatabase()
 
-db.addDevice(1000000000,1,'device1',0,1)
-db.addDevice(1000000000,2,'device1',0,1)
-db.addDevice(1000000001,0,'device2',1,1)
+db.addNode('router','24.52.152.172')
+db.addEmail('trihuynh87@gmail.com')
+db.addEmail('minhtri@uvic.ca')
+db.removeEmail('minhtri@uvic.ca')
+
+db.addDevice(1000000000,0,'Living Lamp',0,1)
+db.addDevice(1000000001,1,'Front Door',1,1)
+db.addDevice(1000000002,1,'Back Door',0,1)
+db.addDevice(1000000003,0,'Front Lamp',1,1)
+db.addDevice(1000000004,0,'Back Lamp',0,1)
+
+
+processEvent(10000000,0)
+processEvent(1000000000,0)
+processEvent(1000000000,1)
+processEvent(1000000001,0)
+processEvent(1000000001,1)
+processEvent(1000000002,0)
+#processEvent(1000000002,1)
+processEvent(1000000003,0)
+processEvent(1000000003,1)
+processEvent(1000000004,0)
+processEvent(1000000004,1)
+
+"""
+# Unit test
+db.addDevice(1000000000,0,'Living Lamp',0,0)
+db.addDevice(1000000001,1,'Front Door',0,0)
+db.addDevice(1000000002,1,'Back Door',1,1)
+
 db.removeDevice(1000000002)
 db.removeDevice(1000000003)
 
@@ -121,3 +248,6 @@ print nodeinfo
 devices=db.getDevices()
 for device in devices:
 	print device
+	
+	
+"""
