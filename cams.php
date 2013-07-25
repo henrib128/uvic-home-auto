1e37c9cde103e6a0e99e2ad582783d5ed45b3053
Henri Balikci <henrib128@gmail.com>
2013-07-24 17:32:03 -0700

    multi cam view

--- cams.php
+++ cams.php
@@ -2,66 +2,41 @@
 <html>
 	<head>
 		<title>Pimation Camera Manager</title>
+		<script type="text/javascript">
+		var frameNr = 0;
+		var loadCnt = 0;
+		var counts = new Array();
+		
+		var size = document.getElementsByTagName('img').length;
+		for(i = 0; i < 2; i++) counts[i] = 0;
+		
+		function refresh(cam, countsIndex) {
+			var img = document.getElementById(cam);
+			
+			counts[countsIndex]++;
+			
+			img.src = "/webcamproxy.php?cam=" + escape(cam) + "&port=8080" + "&n=" + counts[countsIndex];
+		}
+		
+		</script>
 	</head>
-
-	<script type="text/javascript">
-
-	/* Copyright (C) 2007 Richard Atterer, richardÂ©atterer.net
-	   This program is free software; you can redistribute it and/or modify it
-	   under the terms of the GNU General Public License, version 2. See the file
-	   COPYING for details. */
-
-	var imageNr = 0; // Serial number of current image
-	var finished = new Array(); // References to img objects which have finished downloading
-	var paused = false;
-
-	function createImageLayer() {
-	  var img = new Image();
-	  img.style.position = "absolute";
-	  img.style.zIndex = -1;
-	  img.onload = imageOnload;
-	  img.onclick = imageOnclick;
-	  img.src = "/webcamproxy.php?action=snapshot&n=" + (++imageNr) + "&cam=" + escape(document.getElementById("cam_id").value) + "&port=8080";
-	  var webcam = document.getElementById("webcam");
-	  webcam.insertBefore(img, webcam.firstChild);
-	}
-
-	// Two layers are always present (except at the very beginning), to avoid flicker
-	function imageOnload() {
-	  this.style.zIndex = imageNr; // Image finished, bring to front!
-	  while (1 < finished.length) {
-		var del = finished.shift(); // Delete old image(s) from document
-		del.parentNode.removeChild(del);
-	  }
-	  finished.push(this);
-	  if (!paused) createImageLayer();
-	}
-
-	function imageOnclick() { // Clicking on the image will pause the stream
-	  paused = !paused;
-	  if (!paused) createImageLayer();
-	}
-
-	</script>
-
-	<body onload="createImageLayer();">
+	<body>
 		<h1>Welcome to Pimation Camera Manager!</h1>
 		<ul><a href="/pimation.php">Back to Home page</a></ul>
-		
-		<select onchange="createImageLayer();" name="cam" id="cam_id" size=4>
 <?
 		require_once('DBManager.php');
 		db_connect();
 		
 		$result = getCamNamesResult();
 		
+		$i = 0;
 		while($cam = mysql_fetch_array($result)) {
 			$cam = $cam[0];
-			echo "\t\t\t<option value='$cam'>$cam</option>\n";
+			echo "<h1>" . $cam . "</h1><br>\n";
+			echo "<img id='" . $cam . "' src='/blank.jpg' onload=\"refresh(" . $cam . ", " . $i . ");\"/>";
+			$i++;
 		}
 ?>
-		</select>
-		<br><br>
-		<div id="webcam"></div>
+
 	</body>
 </html>
--- webcamproxy.php
+++ webcamproxy.php
@@ -1,12 +1,11 @@
 <?
-if(!isset($_REQUEST['cam']) || !isset($_REQUEST['port']) || !isset($_REQUEST['action']) || !isset($_REQUEST['n'])) exit();
+if(!isset($_REQUEST['cam']) || !isset($_REQUEST['port']) || !isset($_REQUEST['n'])) exit();
 
 require_once('DBManager.php');
 db_connect();
 
-$cam_sel = $_REQUEST['cam'];
-$action = $_REQUEST['action'];
 $n = $_REQUEST['n'];
+$cam_sel = $_REQUEST['cam'];
 $ip = getCamIP($cam_sel);
 $port = $_REQUEST['port'];
 
@@ -16,7 +15,7 @@ $fp = fsockopen($ip, $port, $errno, $errstr, 30);
 if(!$fp) {
 	echo "$errstr ($errno)<br>\n";
 } else {
-	$urlstring = "GET /?action=" . $action . '&n=' . $n . " HTTP/1.0\r\n\r\n";
+	$urlstring = "GET /?action=snapshot&n=" . $n . " HTTP/1.0\r\n\r\n";
 	fputs ($fp, $urlstring);
 	
 	while ($str = trim(fgets($fp, 4096))) {
