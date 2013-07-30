3745bda2519c9e2196d4fa6065d770a5b2a7150d
Tri Huynh <trihuynh87@gmail.com>
2013-07-30 12:36:06 -0700

    added logo to header, change pimation_dir to pi dir

--- header.php
+++ header.php
@@ -1,7 +1,17 @@
 <ul id="menu">
-	<li><a href="/">Home</a></li>
-	<li><a href="/listDevices.php">Devices</a></li>
-	<li><a href="/cams.php">Live Cams</a></li>
-	<li><a href="/playbacks.php">Playbacks</a></li>
+	<table>
+		<tr>
+			<th align="center"><img id="logo" src="/logo.png" alt="PiMation Logo" width="200" height="200"/></th>
+			<th align="left"><font size="7"><span style="color:red;font-weight:bold">Pi</span><span style="color:green;font-weight:bold">Mation</span></font></th>
+		</tr>
+		<tr>
+			<td rowspan=2>
+				<li><a href="/">Home</a></li>
+				<li><a href="/listDevices.php">Devices</a></li>
+				<li><a href="/cams.php">Live Cams</a></li>
+				<li><a href="/playbacks.php">Playbacks</a></li>
+			</td>
+		</tr>
+	</table>
 </ul>
 <div id="main">
--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 		<h1>Welcome to Pimation Home Page!</h1>
 <?
 		# Path to local PiMation working directory
-		#$pimation_dir = '/home/pi/uvic-home-auto/';
-		$pimation_dir="/home/tri/ceng499/uvic-home-auto/";
+		$pimation_dir = '/home/pi/uvic-home-auto/';
+		#$pimation_dir="/home/tri/ceng499/uvic-home-auto/";
 
 		# Pre action to take care of self-direct requests
 		if(isset($_REQUEST['command'])) {
