582dd2739df9d0c2cd8b005aa6cd4013ac1f0eaa
Rolbot <regeisler@gmail.com>
2013-07-30 18:25:33 +0000

    clean /var/www before deploy

--- deploy_web.sh
+++ deploy_web.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
+# Clean /var/www/
+sudo rm /var/www/*
 
+# Copy over revelvant website files
 sudo cp style.css /var/www/
 sudo cp *.php /var/www/
 sudo cp *.jpg /var/www/
+sudo cp *.png /var/www/
 
