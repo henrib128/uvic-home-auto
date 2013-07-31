63cf9b00082c7e04c03261e6fa1c983091484b54
Henri Balikci <henrib128@gmail.com>
2013-07-30 19:38:51 -0700

    https

--- README.md
+++ README.md
@@ -22,3 +22,32 @@ cd pimation
 sudo cp scripts/pimation /etc/init.d/
 sudo chmod 755 /etc/init.d/pimation
 sudo update-rc.d pimation defaults
+
+============================
+
+sudo mkdir /etc/apache2/ssl
+cd /etc/apache2/ssl
+sudo a2ensite default-ssl
+sudo a2enmod ssl
+sudo service apache2 restart
+
+sudo /usr/sbin/make-ssl-cert /usr/share/ssl-cert/ssleay.cnf /etc/apache2/ssl/apache.crt
+# enter public ip for host name and leave others blank
+
+sudo cp apache.crt apache.pem
+sudo cp apache.crt apache.key
+sudo chmod 600 apache.key
+
+sudo nano /etc/apache2/sites-enabled/default-ssl
+# change:
+SSLCertificateFile /etc/ssl/certs/ssl-cert-snakeoil.pem
+SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key
+# to:
+SSLCertificateFile /etc/apache2/ssl/apache.pem
+SSLCertificateKeyFile /etc/apache2/ssl/apache.key
+
+sudo service apache2 restart
+
+============================
+
+
--- web/index.php
+++ web/index.php
@@ -9,45 +9,6 @@
 		<? include("header.php"); ?>
 		
 		<h1>Welcome to Pimation Home Page!</h1>
-<?
-		# Path to local PiMation working directory
-		$pimation_dir = '/opt/pimation/scripts/';
-
-		# Pre action to take care of self-direct requests
-		if(isset($_REQUEST['command'])) {
-			if($_REQUEST['command'] == "startcentral"){
-				# First kill all PiHome and PiCam instance
-				exec("killall -9 PiHome.py");
-				exec("killall -9 PiCam.py");
-			
-				# Then start PiHome and PiCam in the background
-				exec($pimation_dir . "PiCam.py &");
-				exec($pimation_dir . "PiHome.py &");
-			}
-			else if($_REQUEST['command'] == "startremote"){
-				# First kill all PiHome and PiCam instance
-				exec("killall -9 PiHome.py");
-				exec("killall -9 PiCam.py");
-	
-				# Then start PiCam in the background
-				exec($pimation_dir . "PiCam.py &");
-			}
-
-			# Return to self rendering
-			header('Location: ' . $_SERVER['PHP_SELF']);
-		}
-?>
-		<p>Click below to restart your central Pi</p>
-		<form action="<? echo $_SERVER['PHP_SELF']; ?>" method="post">
-			<input type="hidden" name="command" value="NOstartcentral">
-			<input type="submit" value="Restart Central Pi">
-		</form>
-
-		<p>Click below to restart your remote Pi</p>
-		<form action="<? echo $_SERVER['PHP_SELF']; ?>" method="post">
-			<input type="hidden" name="command" value="NOstartremote">
-			<input type="submit" value="Restart Remote Pi">
-		</form>
 		
 		<? include("footer.php"); ?>
 	</body>
